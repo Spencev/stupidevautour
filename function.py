47da68ee8c263160a0690e219998632e1d40dfc3
Spencev <pengrove@hotmail.com>
2018-12-16 17:31:34 -0500

    profile manager

--- function.py
+++ function.py
@@ -1,3 +1,11 @@
+import json
+import profileManager as profile
+
+profileList = profile.loadProfiles()
+userList = profile.genUsers(profileList)
+print(userList)
+profile.newUser("Tiff", profileList)
+
 def generatePercents(playerHand, compHand):
     percentMatrix = []
     for compCard in compHand:
@@ -14,11 +22,11 @@ def generatePercents(playerHand, compHand):
         percentMatrix.append([round((winCount/15)*100, 2), round((tieCount/15)*100, 2), round((loseCount/15)*100, 2)])
     return percentMatrix
 
-ex1 = generatePercents([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15], [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15])
-print(ex1)
-
-statsProfile = open("stats.txt", "r+")
-
-def aversionUpdate(playerHand, playerPlayed, compHand, compPlayed, lastCard):
+#ex1 = generatePercents([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15], [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15])
+#print(ex1)
     
-        
+def calcAversion(playerHand, playerPlayed, lastCard, negMiddleDeck):
+    return abs(playerPlayed * (playerHand.index(playerPlayed) / len(playerHand)) * lastCard * (negMiddleDeck.index(lastCard) / len(negMiddleDeck)) * (playerPlayed - abs(lastCard)))
+
+def calcAggression(playerHand, playerPlayed, lastCard, posMiddleDeck):
+    return playerPlayed * (playerHand.index(playerPlayed) / len(playerHand)) * lastCard * (posMiddleDeck.index(lastCard) / len(posMiddleDeck)) * (playerPlayed - lastCard)
---
+++ profileManager.py
@@ -0,0 +1,37 @@
+import json
+
+def loadProfiles():
+    statsProfiles = open("profiles.txt", "r")
+    profileList = json.load(statsProfiles)
+    print(profileList)
+    return profileList
+
+def genUsers(profileList):
+    userList = []
+    for profile in profileList["profileList"]:
+        userList.append(profile["user"])
+    return userList
+
+def newUser(userInput, profileList):
+    duplicate = False
+    for profile in profileList["profileList"]:
+        if userInput == profile["user"]:
+            print("Duplicate name, try again.")
+            duplicate = True
+    if not duplicate:
+        profileList["profileList"].append({"user":userInput, "aversion":0, "aversionList":[], "aggression":0, "aggressionList":[], "wins": 0, "losses": 0})
+        statsProfiles = open("profiles.txt", "w")
+        statsProfiles.truncate(0)
+        statsProfiles.seek(0)
+        json.dump(profileList, statsProfiles)
+        print("User: " + userInput + " added successfully!")
+
+def selectProfile(nameChosen, profileList):
+    for profile in profileList["profileList"]:
+        if nameChosen == profile["user"]:
+            return profile
+
+def removeProfile(nameChosen, profileList):
+    for profile in profileList["profileList"]:
+        if nameChosen == profile["user"]:
+            profileList["profileList"].remove(profile)
